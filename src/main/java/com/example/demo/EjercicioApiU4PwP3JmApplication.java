a77a891505c4fc73b97f294e90efd4c69b22595c
joselynM99 <joselynmoncayo1999@gmail.com>
2023-02-22 19:47:23 -0500

    taller33

---
+++ src/main/java/com/example/demo/EjercicioApiU4PwP3JmApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioApiU4PwP3JmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioApiU4PwP3JmApplication.class, args);
+	}
+
+}
